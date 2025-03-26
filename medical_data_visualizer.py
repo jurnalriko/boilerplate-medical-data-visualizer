251e3d2f6ee3a8c68c61f9875a74b1b8c4750a7f
jurnalriko <mhmmdrko27@gmail.com>
2025-03-26 16:12:53 +0700

    add solution

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+#df['overweight'] = df['weight'] / (df['height']/100).apply(lambda x: x**2)
+df['bmi'] = df['weight'] / (df['height']/100)**2
+
+def kategori_bmi(bmi):
+    return 1 if bmi > 25 else 0
+
+df['overweight'] = df['bmi'].map(kategori_bmi)
+
+df = df.drop('bmi', axis=1)
 
 # 3
+def normalizing_df(value):
+    if value == 1:
+        return 0
+    elif value > 1:
+        return 1
+    else:
+        return value
+
+df['cholesterol'] = df['cholesterol'].map(normalizing_df)
+df['gluc'] = df['gluc'].map(normalizing_df)
+
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
     
-
+    # 6
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    
     # 7
 
 
-
     # 8
-    fig = None
+    fig = sns.catplot(data = df_cat, x='variable', y='total', hue='value', col='cardio', kind='bar').fig
 
 
     # 9
@@ -38,22 +54,26 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr(method='pearson')
 
     # 13
-    mask = None
-
+    mask = np.triu(corr)
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12,12))
 
     # 15
-
-
+    ax = sns.heatmap(corr, linewidths=1, annot=True, square=True, mask=mask, fmt='.1f', center=0.08, cbar_kws={"shrink":0.5})
 
     # 16
     fig.savefig('heatmap.png')
